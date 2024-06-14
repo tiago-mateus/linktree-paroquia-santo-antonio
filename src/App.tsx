42fbe3037ce8ee7f7c4cf7d1fd29dee2832f2cd9
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-14 02:25:10 -0300

    ajustando css

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Button } from './components/Button'
 function App() {
 
   return (
-    <div className='h-screen py-2 flex flex-col gap-5'>
-    <header className="flex flex-col items-center gap-0.5">
-      <img src="../logo.png" className='w-28' />
-      <p className='text-white font-bold text-2xl'>Paróquia Santo Antônio</p>
-      <p className='text-white font-light italic'>@paroquiaitaitinga</p>
+    <div className='h-screen py-10 flex flex-col gap-7 animate-fadeIn transition-opacity duration-1000'>
+    <header className="flex flex-col items-center gap-3">
+      <img src="../logo.png" className='w-24' />
+      <section className='flex flex-col items-center'>
+        <p className='text-white font-bold text-2xl'>Paróquia Santo Antônio</p>
+        <p className='text-white font-light italic'>@paroquiaitaitinga</p>
+      </section>
     </header> 
     <main className='flex flex-col items-center gap-2'>
       <Button text='Secretaria Paroquial' link='https://wa.me/message/WAVMNOTPCQB3M1'>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,10 +6,8 @@ interface Props {
 export function Button ({text, children, link}: Props){
     return ( 
         <>
-            <button className='bg-white font-bold w-4/5 h-14 animate-fadeIn transition-opacity duration-1000 
-            rounded text-amber-950'>
-                
-                <a href={link} target="_blank" className="flex w-full h-full flex items-center justify-center gap-2">{children} {text}</a>
+            <button className='bg-white font-bold w-4/5 h-14 rounded-lg text-amber-950'>
+                <a href={link} target="_blank" className="flex w-full h-full flex items-center justify-center gap-2 text-xl">{children} {text}</a>
             </button>
         </>
     )
