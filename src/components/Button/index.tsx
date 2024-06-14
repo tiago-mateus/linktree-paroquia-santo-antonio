68af4c62a0cb9db687bb3a0f540f4fd75919e474
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-14 02:13:08 -0300

    ajustando button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,9 +7,9 @@ export function Button ({text, children, link}: Props){
     return ( 
         <>
             <button className='bg-white font-bold w-4/5 h-14 animate-fadeIn transition-opacity duration-1000 
-            rounded text-amber-950 flex items-center justify-center gap-2'>
-                {children}
-                <a href={link} target="_blank">{text}</a>
+            rounded text-amber-950'>
+                
+                <a href={link} target="_blank" className="flex w-full h-full flex items-center justify-center gap-2">{children} {text}</a>
             </button>
         </>
     )
