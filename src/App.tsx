a57fdc1e6f7695239c775d7a931f3e441af563af
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-14 12:44:02 -0300

    ajustando

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ function App() {
 
   return (
     <div className='h-screen py-10 flex flex-col gap-7 animate-fadeIn transition-opacity duration-1000'>
-    <header className="flex flex-col items-center gap-3">
-      <img src="../logo.png" className='w-24' />
+    <header className="flex flex-col items-center gap-1">
+      <div className="bg-white rounded-full instagram">
+        <img src="../logo.png" className='w-24' />
+      </div>
+      
       <section className='flex flex-col items-center'>
         <p className='text-white font-bold text-xl'>Paróquia Santo Antônio</p>
         <p className='text-white font-light italic'>@paroquiaitaitinga</p>
       </section>
     </header> 
-    <main className='flex flex-col items-center gap-3'>
+    <main className='flex flex-col items-center gap-5'>
       <Button text='Secretaria Paroquial' link='https://wa.me/message/WAVMNOTPCQB3M1'>
-      <Send color="#451a03"/>
+        <Send color="#451a03"/>
       </Button>
       <Button text='Youtube' link='https://www.youtube.com/@paroquiasantoantoniodeitai4416'>
-      <Youtube color="#451a03"/>
+        <Youtube color="#451a03"/>
       </Button>
     </main>
     </div>
