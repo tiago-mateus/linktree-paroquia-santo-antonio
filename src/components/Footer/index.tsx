e8ee0d011f3899e41346fecdcda57505d5984d04
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-16 15:38:10 -0300

    ajustes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return(
         <footer className='flex justify-center items-end h-24 animate-fadeInAll'>
-            <img src="../logo-pascom-min.png" className='w-24 opacity-70' />
+            <img src="../logo-pascom-min-2.png" className='opacity-70 w-24 h-6' />
         </footer>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ export function Header(){
     return (
       <header className="flex flex-col items-center animate-fadeInAll">
         <div className="bg-white rounded-full instagram animate-instagramBorder">
-          <img src="../logo-min.png" className='w-24' />
+          <img src="../logo-min-2.png" className='w-24 h-24' />
         </div>      
         <section className='flex flex-col px-5 items-center '>
           <p className='text-white font-bold md:text-xl sm:text-lg text-center'>Paróquia Santo Antônio de Itaitinga</p>
