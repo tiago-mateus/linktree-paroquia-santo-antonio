26d27887c869de550787d177a032993e104795e5
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-08-16 18:28:55 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Facebook, Send, Youtube } from 'lucide-react'
 import { Button } from './components/Button'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
