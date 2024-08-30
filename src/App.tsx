ba1b027402d589d76cceb898ebaaeecca0b27038
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-08-30 17:10:45 -0300

    add calendar

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,58 @@ import { Button } from './components/Button'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import './App.css'
-import { FacebookLogo, WhatsappLogo, YoutubeLogo } from '@phosphor-icons/react'
+import { FacebookLogo,  FilePdf, WhatsappLogo, YoutubeLogo } from '@phosphor-icons/react'
 import { Analytics } from '@vercel/analytics/react'
 
-function App() {
+const buttonData = [
+  {
+    id: 1,
+    text: 'Secretaria Paroquial',
+    link: 'https://wa.me/message/WAVMNOTPCQB3M1',
+    className: 'opacity-0 animate-fadeIn500',
+    icon: <WhatsappLogo size={32} color="#451a03" />,
+  },
+  {
+    id: 2,
+    text: 'Youtube',
+    link: 'https://www.youtube.com/@paroquiasantoantoniodeitai4416',
+    className: 'opacity-0 animate-fadeIn1000',
+    icon: <YoutubeLogo size={32} color="#451a03" />,
+  },
+  {
+    id: 3,
+    text: 'Facebook',
+    link: 'https://www.facebook.com/profile.php?id=100008817789375',
+    className: 'opacity-0 animate-fadeIn1500',
+    icon: <FacebookLogo size={32} color="#451a03" />,
+  },
+  {
+    id: 4,
+    text: 'Calendário do Mês',
+    link: '../.../public/Calendario_de_Setembro_2024.pdf',
+    className: 'opacity-0 animate-fadeIn1500',
+    icon: <FilePdf size={32} color="#451a03" />,
+  }
+]
+
+export default function App(){
   return (
-  <div className='flex flex-col py-5 gap-5'>
-    <Header />
-    <main className='flex flex-col items-center gap-3'>
-      <Button 
-        text='Secretaria Paroquial' 
-        link='https://wa.me/message/WAVMNOTPCQB3M1'
-        className='opacity-0 animate-fadeIn500'
-        >
-        <WhatsappLogo size={32}  color="#451a03" />
-      </Button>
-      <Button 
-        text='Youtube'
-        link='https://www.youtube.com/@paroquiasantoantoniodeitai4416'
-        className='opacity-0 animate-fadeIn1000'
-        >
-          <YoutubeLogo size={32} color="#451a03"/>
-      </Button>
-      <Button 
-        text='Facebook' 
-        link='https://www.facebook.com/profile.php?id=100008817789375'
-        className='opacity-0 animate-fadeIn1500'
-        >
-        <FacebookLogo size={32} color="#451a03"/>
-      </Button>
-    </main>
-    <Footer/>
-    <Analytics />
-  </div>
+    <div className='flex flex-col py-5 gap-5'>
+      <Header />
+      <main className='flex flex-col items-center gap-3'>
+        {buttonData.map(({ text, link, className, icon, id }) => (
+          <Button 
+            key={id}
+            text={text}
+            link={link}
+            className={className}
+          >
+            {icon}
+          </Button>
+        ))}
+      </main>
+      <Footer />
+      <Analytics />
+    </div>
   )
 }
-
-export default App
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight } from "@phosphor-icons/react"
+import { ArrowUpRight, DownloadSimple } from "@phosphor-icons/react"
 
 interface Props {
     text: string
@@ -7,10 +7,28 @@ interface Props {
     className?: string // Adiciona a propriedade className opcional
 }
 export function Button ({text, children, link, className}: Props){
+    const isPdf = link.endsWith('.pdf');
+
+  const handleClick = () => {
+    if (isPdf) {
+        const linkElement = document.createElement('a');
+        if (link) {
+            linkElement.href = link;
+            linkElement.download = link.split('/').pop() || '';
+        }
+      document.body.appendChild(linkElement);
+      linkElement.click();
+      document.body.removeChild(linkElement);
+    }
+  };
+
     return ( 
         <>
-            <button className={`bg-white font-bold w-11/12 h-16 rounded-2xl text-amber-950 ${className}`}>
-                <a href={link} target="_blank" className="flex w-full h-full flex justify-between items-center px-5 gap-2 text-xl"><span className="flex items-center gap-3">{children} {text}</span> <ArrowUpRight size={15} weight="bold"/></a>
+            <button onClick={isPdf ? handleClick : undefined} className={`bg-white font-bold w-11/12 h-16 rounded-2xl text-amber-950 ${className}`}>
+                <a href={isPdf ? "#" : link} target={isPdf ? "" : "_blank"} 
+                className="flex w-full h-full flex justify-between items-center px-5 gap-2 text-xl">
+                    <span className="flex items-center gap-3">{children} {text}</span> 
+                    {isPdf ? (<DownloadSimple size={15} weight="bold"/>)  :(<ArrowUpRight size={15} weight="bold"/>) }</a>
             </button>
         </>
     )
